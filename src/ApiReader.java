f19cc8d3b8dc8bdef1a00d3f460cf37281428cbf
offjuly <baumgartnerj19000@sus.htl-grieskirchen.at>
2021-10-11 10:29:23 +0200

    second commit

--- src/ApiReader.java
+++ src/ApiReader.java
@@ -1,18 +1,37 @@
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ApiReader {
-    BufferedReader br;
 
-    {
-        try {
-            br = new BufferedReader(new FileReader("numbers.csv"));
-            String line = br.readLine();
-            
 
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+    List myList = new ArrayList<>();
+
+    public void readIn(){
+        {
+            try {
+                BufferedReader br = new BufferedReader(new FileReader("numbers.csv"));
+                String line = br.readLine();
+
+                while(line != null){
+                    String[] split = line.split(":");
+                    for (int i = 0; i < split.length; i++) {
+                        System.out.println(split[i]);
+                        myList.add(split[i]);
+                    }
+
+
+                }
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
+
 }
