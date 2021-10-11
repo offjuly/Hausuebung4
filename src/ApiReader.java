756fb1b2943e32050298129afbb505d5034223be
offjuly <baumgartnerj19000@sus.htl-grieskirchen.at>
2021-10-11 12:27:57 +0200

    third commit

--- src/ApiReader.java
+++ src/ApiReader.java
@@ -4,25 +4,40 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class ApiReader {
-
-
     List myList = new ArrayList<>();
 
-    public void readIn(){
+    public void readIn() {
         {
+            Scanner scanner = new Scanner(System.in);
             try {
                 BufferedReader br = new BufferedReader(new FileReader("numbers.csv"));
-                String line = br.readLine();
-
-                while(line != null){
+                String line ;
+                boolean running = true;
+                while ((line = br.readLine()) != null) {
                     String[] split = line.split(":");
                     for (int i = 0; i < split.length; i++) {
                         System.out.println(split[i]);
-                        myList.add(split[i]);
-                    }
+                        running = true;
+                        try {
+                           int j= Integer.parseInt(split[i]);
+                        }catch( NumberFormatException e){
+                            running = false;
+                        }
+                        if(running == true){
+                            myList.add(Integer.parseInt(split[i]));
 
+                        }
+                        System.out.println(myList.size());
+
+                        //int userEingabe = scanner.nextInt();
+
+                        //if (Integer.parseInt(split[i]) % userEingabe == 0) {
+                          //  System.out.println(split[i]);
+                        //}
+                    }
 
                 }
 
@@ -33,5 +48,6 @@ public class ApiReader {
             }
         }
     }
-
 }
+
+
